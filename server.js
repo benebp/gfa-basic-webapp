0a25ca8048fa489e9e2031467ffde53907a7d2ca
benebp <benebp@github.com>
2020-03-16 16:45:38 +0100

    mod3

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('MOD 1: Welcome to this useless webapp. I have no other function, just to display this message to you.');
+  res.send('MOD 3: Welcome to this useless webapp. I have no other function, just to display this message to you.');
 });
 
 app.listen(PORT);
