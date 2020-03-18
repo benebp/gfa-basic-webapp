0cc5521861db5ad6919df0db9666af27a12efc59
benebp <benebp@github.com>
2020-03-18 16:27:25 +0100

    mod20

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('MOD19: malacok-27, new branch "dev" created');
+  res.send('MOD20: malacok-27, new branch "dev" created');
 });
 
 app.listen(PORT);
